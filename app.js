ea17c5aa4d549eacb6bd181c7ad2ffc5daecc2e0
Jiyong Sim <sjy19910222@gmail.com>
2021-11-30 22:51:53 +0900

    #2.1 2D Context

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,9 +10,20 @@ function stopPainting () {
     painting = false;
 };
 
+function startPainting () {
+    painting = true;
+};
+
 function onMouseMove (event) {
     const x = event.offsetX;
     const y = event.offsetY;
+    if (!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    };
 
 };
 
@@ -16,17 +31,10 @@ function onMouseDown (event) {
     painting = true;
 };
 
-function onMouseUp (event) {
-    stopPainting();
-};
-function onMouseLeave () {
-    stopPainting();
-};
-
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
-    canvas.addEventListener("mouseleave", onMouseLeave);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
 
 };
