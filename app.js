837f8503b2e7bce90fb25e7da3d81e79a9dedc60
Jiyong Sim <sjy19910222@gmail.com>
2021-12-01 14:36:22 +0900

    #2.6 Saving the Image

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "2c2c2c";
 const CANVAS_SIZE = "700";
 
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 
 ctx.strokeStyle = INITIAL_COLOR;    //그릴 선들의 색깔
 ctx.fillStyle = INITIAL_COLOR;      //채울 색깔
@@ -43,12 +47,26 @@ function handleCanvasClick () {
     };
 };
 
+function handleCM (event) {
+    event.preventDefault();
+};
+
+function handleSaveClick () {
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS[EXPORT]";
+    console.log(link);
+    link.click();
+};
+
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
     canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
 };
 
 function handleColorClick (event) {
@@ -82,4 +100,8 @@ if (range) {
 //Fill 버튼을 눌르면 canvas전체에 지정한 색깔로 변경 가능
 if (mode) {
     mode.addEventListener("click", handleModeClick);
+};
+
+if (saveBtn) {
+    saveBtn.addEventListener("click", handleSaveClick)
 };
