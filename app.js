a9bed6750ddf43a65bb866c1f768d726e638ed0d
Jiyong Sim <sjy19910222@gmail.com>
2021-12-01 14:12:15 +0900

    #2.5 Filling Mode

--- app.js
+++ app.js
@@ -4,14 +4,18 @@ const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 
-canvas.width = 700;
-canvas.height = 700;
+const INITIAL_COLOR = "2c2c2c";
+const CANVAS_SIZE = "700";
 
-ctx.strokeStyle = "#2c2c2c";    //그릴 선들의 색깔
-ctx.lineWidth = 2.5;            //선의 너비
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
 
-let painting = false;
-let filling = false;
+ctx.strokeStyle = INITIAL_COLOR;    //그릴 선들의 색깔
+ctx.fillStyle = INITIAL_COLOR;      //채울 색깔
+ctx.lineWidth = 2.5;                //선의 너비
+
+let painting = false;               //색깔
+let filling = false;                //색깔 채우기
 
 function stopPainting () {
     painting = false;
@@ -31,7 +35,12 @@ function onMouseMove (event) {
         ctx.lineTo(x, y);
         ctx.stroke();
     };
+};
 
+function handleCanvasClick () {
+    if (filling) {
+        ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    };
 };
 
 if (canvas) {
@@ -39,13 +48,13 @@ if (canvas) {
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
-
+    canvas.addEventListener("click", handleCanvasClick);
 };
 
 function handleColorClick (event) {
     const color = event.target.style.backgroundColor;
-    console.log(color);
     ctx.strokeStyle = color;
+    ctx.fillStyle = ctx.strokeStyle;
 };
 
 function handleRangeChange (event) {
@@ -65,10 +74,12 @@ function handleModeClick () {
 
 Array.from(colors).forEach(color =>color.addEventListener("click", handleColorClick));
 
+// range 스크롤을 조절하면, 선의 너비가 변경
 if (range) {
     range.addEventListener("input", handleRangeChange);
 };
 
+//Fill 버튼을 눌르면 canvas전체에 지정한 색깔로 변경 가능
 if (mode) {
     mode.addEventListener("click", handleModeClick);
 };
